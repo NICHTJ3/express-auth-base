850bccd364bd2818728f904004c585d681818f14
NICHTJ3 <nicholsontrent@gmail.com>
2020-05-12 17:29:45 +1200

    Rename auth controller

--- src/api/auth/router.js
+++ src/api/auth/router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const AuthController = require('./controllers/authController');
+const AuthController = require('./controllers/auth.controller');
 const RequestSchemas = require('./schemas');
 
 const router = express.Router();
